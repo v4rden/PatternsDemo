39600b63dc62fa7c2e511181fc97b4480d564e92
v4rden <denys.variah@gmail.com>
2019-07-07 15:27:58 +0300

    Initial commit

---
+++ PatternsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
